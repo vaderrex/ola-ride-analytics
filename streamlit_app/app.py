0ab24c4f31806bd6379fa5ab449b0f4089f50fa5
vaderrex <129682963+vaderrex@users.noreply.github.com>
2026-01-07 21:48:53 +0530

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -5,7 +5,7 @@ import pandas as pd
 # LOAD LOGO
 # =====================================
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-LOGO_PATH = os.path.join(BASE_DIR, "assets", "ola_logo.png")
+LOGO_PATH = os.path.join(BASE_DIR, "assets","ola_logo.png")
 # =====================================
 # PAGE CONFIG (MUST BE FIRST STREAMLIT CALL)
 # =====================================
@@ -159,7 +159,7 @@ with st.sidebar:
         """,
         unsafe_allow_html=True
     )
-    st.image("ola_logo.png", width=500)
+    st.image("ola_logo.png", width=300)
   
 
     st.subheader("🔍 Filters")
